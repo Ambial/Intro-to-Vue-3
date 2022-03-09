4213cf2897c1250c522b1751c697a49e70a28d28
Simon Koerner <simon.koerner@nexenio.com>
2022-03-09 15:30:10 +0100

    Lesson 8 solution incl challenge

--- main.js
+++ main.js
@@ -4,21 +4,39 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: true },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false },
+            ],
+            activeVariantIndex: 0
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariantByIndex(index) {
+            if (index !== this.activeVariantIndex) {
+                this.activeVariantIndex = index   
+            }
+        }
+    },
+    computed: {
+        title(){
+            return `${this.brand} ${this.product}`
+        },
+        variantImage(){
+            return this.variants[this.activeVariantIndex].image
+        },
+        inStock(){
+            return this.variants[this.activeVariantIndex].quantity > 0
+        },
+        saleMessage() {
+            return `${this.title} is on sale!` 
+        },
+        onSale() {
+            return this.variants[this.activeVariantIndex].onSale && this.inStock
         }
     }
 })
